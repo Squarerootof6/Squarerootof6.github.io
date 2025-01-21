dde1d8af69b0c7b71391bb9e3fd241b583c421d3
Genghao Liu <Squarerootof6@outlook.com>
2025-01-21 10:36:39 +0800

    new research page
    optimize structure and revise the layout of the sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -258,5 +258,97 @@
 				});
 
 			});
+            
 
+        //Gallery
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ '481px',   '736px'  ],
+			xsmall:  [ null,      '480px'  ]
+		});
+
+	// Hack: Enable IE workarounds.
+		if (browser.name == 'ie')
+			$body.addClass('ie');
+
+	// Touch?
+		if (browser.mobile)
+			$body.addClass('touch');
+
+	// Transitions supported?
+		if (browser.canUse('transition')) {
+
+			// Play initial animations on page load.
+				var $album = $('#album');
+				$album.ready(function() {
+					window.setTimeout(function() {
+						$body.removeClass('is-preload');
+					}, 100);
+				});
+
+			// Prevent transitions/animations on resize.
+				var resizeTimeout;
+
+				$window.on('resize', function() {
+
+					window.clearTimeout(resizeTimeout);
+
+					$body.addClass('is-resizing');
+
+					resizeTimeout = window.setTimeout(function() {
+						$body.removeClass('is-resizing');
+						
+					}, 100);
+
+				});
+        }
+        var $album = $('#album');
+        $album.ready(function() {
+            // Poptrox.
+			$album.poptrox({
+				baseZIndex: 20000,
+				caption: function($a) {
+					var s = '';
+					$a.nextAll().not('ul').each(function() {
+						s += this.outerHTML;
+					});
+					return s;
+				},
+				fadeSpeed: 300,
+				onPopupClose: function() { $body.removeClass('modal-active'); },
+				onPopupOpen: function() { $body.addClass('modal-active'); },
+				overlayOpacity: 0.5,
+				popupCloserText: '',
+				popupHeight: 150,
+				popupLoaderText: 'loading',
+				popupSpeed: 300,
+				popupWidth: 150,
+				selector: '.thumb > a.image',
+				usePopupCaption: true,
+				usePopupCloser: true,
+				usePopupDefaultStyling: false,
+				usePopupForceClose: true,
+				usePopupLoader: true,
+				usePopupNav: true,
+				windowMargin: 50
+			});
+			// Hack: Set margins to 0 when 'xsmall' activates.
+			setTimeout(function() {
+                if ($album[0]._poptrox) {
+                    // Hack: 根据 breakpoint 设置 margin
+                    breakpoints.on('<=xsmall', function() {
+                        $album[0]._poptrox.windowMargin = 0;
+                    });
+        
+                    breakpoints.on('>xsmall', function() {
+                        $album[0]._poptrox.windowMargin = 50;
+                    });
+                } else {
+                    console.log("Poptrox 未初始化！");
+                }
+            }, 300);
+        });
 })(jQuery);
